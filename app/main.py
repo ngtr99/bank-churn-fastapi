b8ffa1bb799414b3cf640e73938e628cf964207b
ngtr99 <minhnguyet12923@gmail.com>
2026-01-15 15:41:55 -0800

    add pandas

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
-import numpy as np
+import pandas as pd
 
 from app.schemas import ChurnFeatures
 from app.model_loader import model
@@ -16,35 +16,19 @@ app = FastAPI(title="Customer Churn Prediction API",
               description="An API that predicts customer churn using a pre-trained model.",
               version="1.0.0")
 
-@app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
-    return Response(status_code=204)
 
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+
 @app.get("/", response_class=HTMLResponse)
 def web(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-
-
-@app.post("/predict/")
+@app.post("/predict")
 def predict(payload: ChurnFeatures):
-    X = np.array([[
-        payload.customer_id,
-        payload.credit_score,
-        payload.country,
-        payload.gender,
-        payload.age,
-        payload.tenure,
-        payload.balance,
-        payload.products_number,
-        payload.credit_card,
-        payload.active_member,
-        payload.estimated_salary
-        ]], dtype=object)
+    X = pd.DataFrame([payload.dict()])
 
     try: 
         pred = model.predict(X)[0]
